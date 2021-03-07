92564a1497d24f3f6caf8cc27952ad6ad427ffb5
김민수 <kms@Kbeehive-MacBookPro.local>
2021-03-07 15:59:25 +0900

    test for kokbee

--- lotto.go
+++ lotto.go
@@ -8,11 +8,17 @@ import (
 
 func main() {
 	flagBae := flag.Bool("bae", false, "name of bae")
+	flagkok := flag.Bool("kok", false, "nink of kokbee")
+
 	flag.Parse()
 
 	if *flagBae {
 		fmt.Println("bae seo young")
 		os.Exit(0)
+	else if *flagkok {
+		fmt.Println("nickname kokbee")
+		os.Exit(0)
+	}
 	} else {
 		flag.Usage()
 		os.Exit(1)
