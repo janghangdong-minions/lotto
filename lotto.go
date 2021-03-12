d379a61195c96ed9a1a5ef7696d15cc679171f34
Cpocari <klhs1207@naver.com>
2021-03-12 18:46:32 +0900

    addFlagMan

--- lotto.go
+++ lotto.go
@@ -10,6 +10,7 @@ var (
 	flagBae = flag.Bool("bae", false, "name of bae")
 	flagSol = flag.Bool("sol", false, "name of solip")
 	flagkok = flag.Bool("kok", false, "nick of kokbee")
+	flagMan = flag.Bool("Man", false, "name of ManJe")
 )
 
 func main() {
@@ -20,6 +21,8 @@ func main() {
 		os.Exit(0)
 	} else if *flagSol {
 		fmt.Println("lim sol Leaf")
+	} else if *flagMan {
+		fmt.Println("Kang Man JE")
 	} else if *flagkok {
 		fmt.Println("nickname kokbee")
 		os.Exit(0)
