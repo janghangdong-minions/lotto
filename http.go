ea2f29afe6eec89731254303d5a1151ed14e7dd4
pokari <pokari@woong-book.local>
2021-03-14 15:30:37 +0900

    addMan

--- http.go
+++ http.go
@@ -9,6 +9,7 @@ import (
 func webserver() {
 	http.HandleFunc("/", handleInit)
 	http.HandleFunc("/bae", handleBae)
+	http.HandleFunc("/Man", handleMan)
 
 	err := http.ListenAndServe(*flagHTTPPort, nil) //nil의 정확한 용도를 이슈로 만들어봅시당.
 	if err != nil {
@@ -22,4 +23,8 @@ func handleInit(w http.ResponseWriter, r *http.Request) {
 
 func handleBae(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "hello, %s", "bae")
+}
+
+func handleMan(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello, %s", "Man")
 }
