975a301cae2bd5da490ead92a5c434d8899b8046
Bailey <bsy2658@gmail.com>
2021-03-14 14:57:12 +0900

    webserver

---
+++ http.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func webserver() {
+	http.HandleFunc("/", handleInit)
+	http.HandleFunc("/bae", handleBae)
+
+	err := http.ListenAndServe(*flagHTTPPort, nil) //nil의 정확한 용도를 이슈로 만들어봅시당.
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func handleInit(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "hello world")
+}
+
+func handleBae(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello, %s", "bae")
+}
---
+++ network.go
@@ -0,0 +1,42 @@
+package main
+
+import "net"
+
+func serviceIP() (string, error) {
+	ip := "127.0.0.1"
+	ifaces, err := net.Interfaces() // 네트워크 인터페이스의 목록을 반환한다.
+	if err != nil {
+		return ip, err
+	}
+	//https://golang.org/src/net/interface.go?s=2954:2992#L89
+	for _, iface := range ifaces {
+		if iface.Flags&net.FlagUp == 0 {
+			continue //interface down
+		}
+		if iface.Flags&net.FlagLoopback != 0 {
+			continue // loopback interface
+		}
+		addrs, err := iface.Addrs()
+		if err != nil {
+			return ip, err
+		}
+		for _, addr := range addrs {
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+			if ip == nil || ip.IsLoopback() {
+				continue
+			}
+			ip = ip.To4()
+			if ip == nil {
+				continue // not an ipv4 address
+			}
+			return ip.String(), nil
+		}
+	}
+	return ip, nil
+}
