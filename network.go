1990139e39a1e723d744668f317a3ece348b5d97
idea <idea@localhost.localdomain>
2021-03-15 14:35:30 +0900

    net 주석

--- network.go
+++ network.go
@@ -1,21 +1,27 @@
 package main
 
-import "net"
+import (
+	"net"
+)
 
 func serviceIP() (string, error) {
 	ip := "127.0.0.1"
-	ifaces, err := net.Interfaces() // 네트워크 인터페이스의 목록을 반환한다.
+	ifaces, err := net.Interfaces() // 네트워크 인터페이스의 목록을 반환한다. ifconfig, ipconfig의 출력과 같다.
 	if err != nil {
 		return ip, err
 	}
+	// for 문에서 모든 리스트가 아닌 일부리스트만 반환하는지는 모르겟어요.
 	//https://golang.org/src/net/interface.go?s=2954:2992#L89
 	for _, iface := range ifaces {
+		// 네트워크는 up & down 으로 끄고 킬 수 있으므로, down되어 있는지 체크하는 조건문
 		if iface.Flags&net.FlagUp == 0 {
 			continue //interface down
 		}
+		// A 클래스 ip 자기자신의 ip를 루프백이라고 합니다. 자기자신은 제외하는 조건문
 		if iface.Flags&net.FlagLoopback != 0 {
 			continue // loopback interface
 		}
+		// ip address 반환
 		addrs, err := iface.Addrs()
 		if err != nil {
 			return ip, err
@@ -31,6 +37,7 @@ func serviceIP() (string, error) {
 			if ip == nil || ip.IsLoopback() {
 				continue
 			}
+			// ipv4로 체크
 			ip = ip.To4()
 			if ip == nil {
 				continue // not an ipv4 address
