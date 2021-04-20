92422d9196e48a0839cd04a46143991489e09990
pokari <pokari@woong-book.local>
2021-04-20 20:13:34 +0900

    http clear code

--- http.go
+++ http.go
@@ -8,10 +8,6 @@ import (
 
 func webserver() {
 	http.HandleFunc("/", handleInit)
-	http.HandleFunc("/bae", handleBae)
-	http.HandleFunc("/man", handleMan)
-	http.HandleFunc("/sol", handleSol)
-	http.HandleFunc("/kok", handleKok)
 
 	fs := http.FileServer(http.Dir("assets/"))
 	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
@@ -32,62 +28,6 @@ func handleInit(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(err)
 	}
 	err = i.ExecuteTemplate(w, "init", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func handleBae(w http.ResponseWriter, r *http.Request) {
-	i, err := template.ParseFiles(
-		"assets/html/header.html",
-		"assets/html/footer.html",
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = i.ExecuteTemplate(w, "bae", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func handleMan(w http.ResponseWriter, r *http.Request) {
-	i, err := template.ParseFiles(
-		"assets/html/header.html",
-		"assets/html/footer.html",
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = i.ExecuteTemplate(w, "man", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func handleSol(w http.ResponseWriter, r *http.Request) {
-	i, err := template.ParseFiles(
-		"assets/html/header.html",
-		"assets/html/footer.html",
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = i.ExecuteTemplate(w, "sol", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func handleKok(w http.ResponseWriter, r *http.Request) {
-	i, err := template.ParseFiles(
-		"assets/html/header.html",
-		"assets/html/footer.html",
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = i.ExecuteTemplate(w, "kok", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
