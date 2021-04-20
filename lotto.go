f7f54c44ba82e0b1409c61317f462b38428ce9a4
di <di@diui-MacBookPro.local>
2021-04-20 20:17:46 +0900

    update random function

--- lotto.go
+++ lotto.go
@@ -11,13 +11,15 @@ import (
 var (
 	flagHTTPPort = flag.String("http", "", "webservice port number")
 	flagGenNum   = flag.Bool("gennum", false, "generating random numbers")
+	flagRangeMin = flag.Int("rmin", 1, "generating random numbers for min value")
+	flagRangeMax = flag.Int("rmax", 45, "generating random numbers for max value")
+	flagRange    = flag.Int("r", 3, "generating random numbers for range")
 )
 
-func GenRandomNums() {
+// GenRandomNums 랜덤 값 출력
+func GenRandomNums(min int, max int, ranges int) {
 	// 로또 1~45까지 숫자 중 7개의 숫자를 뽑는다.
-	min := 1
-	max := 45
-	for i := 0; i < 7; i++ {
+	for i := min; i < ranges+1; i++ {
 		fmt.Println(rand.Intn(max-min) + min)
 	}
 
@@ -34,7 +36,16 @@ func main() {
 		fmt.Printf("Service start: http://%s%s\n", ip, *flagHTTPPort)
 		webserver()
 	} else if *flagGenNum {
-		GenRandomNums()
+		if *flagRangeMin <= 0 {
+			fmt.Println("min값은 1이상 입력해주세요")
+		}
+		if *flagRangeMax < 45 {
+			fmt.Println("max값은 45이상 입력해주세요")
+		}
+		if *flagRange < 1 {
+			fmt.Println("range값을 1이상 입력해주세요")
+		}
+		GenRandomNums(*flagRangeMin, *flagRangeMax, *flagRange)
 	} else {
 		flag.Usage()
 		os.Exit(1)
