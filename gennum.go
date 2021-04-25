8272b467f2f9a87fd908c4b1f1bf3e7da02006cb
Bailey <bsy2658@gmail.com>
2021-04-25 15:49:05 +0900

    기능 버그 해결(리팩토링 필요)

--- gennum.go
+++ gennum.go
@@ -9,6 +9,7 @@ import (
 func GenRandomNums(min, max, ranges int) LottoNum {
 	// 로또 1~45까지 숫자 중 7개의 숫자를 뽑는다.
 	var nums []int
+	max = max + 1
 	for i := 0; i < ranges; i++ {
 		rand.Seed(time.Now().UnixNano())
 		nums = append(nums, rand.Intn(max-min)+min)
