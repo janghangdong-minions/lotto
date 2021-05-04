362243f2ee2917d922c9a2a183dbe3f9de72849f
soluleaf <soluleaf@gmail.com>
2021-05-04 20:01:12 +0900

    paddingNum 리펙토링

--- templatefunc.go
+++ templatefunc.go
@@ -1,12 +1,11 @@
 package main
 
-import "strconv"
+import (
+	"fmt"
+)
 
 //PaddingNum 함수는 숫자가 한 자리일 때 앞에 0을 붙여주는 template 함수이다.
 func PaddingNum(num int) string {
-	n := strconv.Itoa(num)
-	for len(n) < 2 {
-		n = "0" + n
-	}
+	n := fmt.Sprintf("%02d", num)
 	return n
 }
