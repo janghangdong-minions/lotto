ae03facfa92113dd6ec400edccbfe4b9ff77db0c
soluleaf <soluleaf@gmail.com>
2021-05-31 18:14:16 +0900

    numColorSet

--- lotto.go
+++ lotto.go
@@ -2,9 +2,6 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"log"
-	"os"
 )
 
 var (
@@ -16,31 +13,5 @@ var (
 )
 
 func main() {
-	flag.Parse()
-
-	if *flagHTTPPort != "" {
-		if *flagRange < 1 {
-			fmt.Println("range값을 1이상 입력해주세요")
-		}
-		if *flagRangeMax <= *flagRangeMin {
-			fmt.Println("rmax값은 rmin값보다 큰 값을 입력해주세요")
-		}
-		ip, err := serviceIP()
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("Service start: http://%s%s\n", ip, *flagHTTPPort)
-		webserver()
-	} else if *flagGenNum {
-		if *flagRange < 1 {
-			fmt.Println("range값을 1이상 입력해주세요")
-		}
-		if *flagRangeMax <= *flagRangeMin {
-			fmt.Println("rmax값은 rmin값보다 큰 값을 입력해주세요")
-		}
-		GenRandomNums(*flagRangeMin, *flagRangeMax, *flagRange)
-	} else {
-		flag.Usage()
-		os.Exit(1)
-	}
+	getFlag()
 }
