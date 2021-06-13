af663bae8524c7425b73880bc63b4750f7541c30
Bailey <bsy2658@gmail.com>
2021-06-13 14:27:59 +0900

    flag.go 삭제 및 i를 obj로 변경

--- assets/js/lotto.js
+++ assets/js/lotto.js
@@ -2,18 +2,18 @@
 function setNumberColor(){
     numObj = document.getElementsByClassName('circle');
     
-    for(let i=0; i<numObj.length; i++){
-        num = numObj[i].textContent
+    for(let obj=0; obj<numObj.length; obj++){
+        num = numObj[obj].textContent
         if (num<=10){
-            numObj[i].style.color = 'yellow';
+            numObj[obj].style.color = 'yellow';
         }else if(num>10 && num<=20){
-            numObj[i].style.color = 'blue';
+            numObj[obj].style.color = 'blue';
         }else if(num>20 && num<=30){
-            numObj[i].style.color = 'red';
+            numObj[obj].style.color = 'red';
         }else if(num>30 && num<=40){
-            numObj[i].style.color = 'black';
+            numObj[obj].style.color = 'black';
         }else if(num>40 && num<=50){
-            numObj[i].style.color = 'green';
+            numObj[obj].style.color = 'green';
         }
     }
 }
