4a80093d5224e7a672808c6453f4dd7b30957043
soluleaf <soluleaf@gmail.com>
2021-07-04 14:37:15 +0900

    editgennum 수정

--- gennum.go
+++ gennum.go
@@ -6,7 +6,7 @@ import (
 )
 
 // GenRandomNums 랜덤 값 출력
-func GenRandomNums(min, max, ranges int) *LottoNum {
+func GenRandomNums(min, max, ranges int) LottoNum {
 	// 로또 1~45까지 숫자 중 7개의 숫자를 뽑는다.
 	var nums []int
 	for i := 0; i < ranges; i++ {
@@ -14,5 +14,5 @@ func GenRandomNums(min, max, ranges int) *LottoNum {
 		nums = append(nums, rand.Intn(max-min+1)+min)
 	}
 	l := LottoNum{nums}
-	return &l
+	return l
 }
