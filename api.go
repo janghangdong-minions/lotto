d11ac647c1c03ebdfcd233e7938087de9c48b590
soluleaf <soluleaf@gmail.com>
2021-08-08 14:59:44 +0900

    renameApiFunc

--- api.go
+++ api.go
@@ -5,7 +5,11 @@ import (
 	"net/http"
 )
 
-func handleRandom(w http.ResponseWriter, r *http.Request) {
+func GetRandomnum(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "이함수는 GET메소드만 지원합니다.", http.StatusBadRequest)
+		return
+	}
 	nums := GenRandomNums(*flagRangeMin, *flagRangeMax, *flagRange)
 	data, err := json.Marshal(nums)
 	if err != nil {
